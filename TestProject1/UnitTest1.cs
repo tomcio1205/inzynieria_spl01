ce46f3330f3f846632389a43962508b7c62441dc
Tomasz Rajzer <trajzer@wsiz.edu.pl>
2023-03-06 16:01:46 +0100

    Add models

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,4 +1,7 @@
+using Bogus;
+using Bogus.DataSets;
 using System;
+using TestProject1.Models;
 using Xunit;
 
 namespace TestProject1
@@ -8,7 +11,15 @@ namespace TestProject1
         [Fact]
         public void Test1()
         {
-
+            Faker<UserAddress> addressBogus = new Faker<UserAddress>();
+            addressBogus.RuleFor(x => x.Description, y => y.Address.FullAddress());
+                addressBogus.RuleFor(x => x.Number, y => y.Address.CountryCode());
+            var personBogus = new Faker<Models.Person>();
+            personBogus.RuleFor(x => x.Name, y => y.Person.FirstName);
+            personBogus.RuleFor(x => x.Surname, y => y.Person.LastName);
+            personBogus.RuleFor(x => x.Birthday, y => y.Person.DateOfBirth);
+            personBogus.RuleFor(x => x.Address, y => addressBogus.Generate());
+            var hundredPpl = personBogus.Generate(100);
         }
     }
 }
